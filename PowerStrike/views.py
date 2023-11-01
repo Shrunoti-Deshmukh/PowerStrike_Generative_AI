e02516e80a17c584872da9bdac248dfaa7a4a629
Shrunoti-Deshmukh <71890142+Shrunoti-Deshmukh@users.noreply.github.com>
2023-11-01 12:40:12 +0530

    Add files via upload

--- PowerStrike/views.py
+++ PowerStrike/views.py
@@ -5,7 +5,13 @@ import random
 import io
 import base64
 from PIL import Image
-import cv2
+from django.template.defaulttags import register
+
+
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
 
 def index(request):
     if request.method == "POST":
@@ -29,7 +35,7 @@ def index(request):
         attack_names = responce.split('\n')
         attacks = []
         images = {}
-        img_list = []
+        img_list = {}
         for attack in attack_names:
             attack_without_number = attack[3:]
             attacks.append(attack_without_number)
@@ -52,13 +58,13 @@ def index(request):
                 temperature=1,
                 max_output_tokens=800
             )
-            print(completion.result)
             import requests
 
             API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
             headers = {"Authorization": f"Bearer {'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'}"}
 
-            prompt = "The Pink Ranger's power stone is an exquisite gem, imbued with the essence of love and courage. Its delicate shade of soft pink reflects her unwavering determination and compassion. When harnessed, it grants her the ability to conjure protective barriers and healing energy, embodying the enduring power of love in the face of darkness."
+            print(completion.result)
+            prompt = completion.result
             def query(payload):
                 response = requests.post(API_URL, headers=headers, json=payload)
                 return response.content
@@ -67,29 +73,17 @@ def index(request):
                 "guidance_scale": 7.5,
                 "num_inference_steps": 200,
                 "denoising_strength": 0.8,
-            }).decode('latin-1')
-            
-            # image = Image.open(io.BytesIO(image_bytes))
-            # # Upscale the image to 512x512 pixels
-            # image = image.resize((1024, 1024))
-            
-            # name = i+".jpg"
-            # print(type(image))
+            })
 
-            # with io.BytesIO() as buffer:
-            #     image.save(buffer,format="JPEG")
-            #     image_bytes = buffer.getvalue()
+            img_list[i] = base64.b64encode(io.BytesIO(image_bytes).getvalue()).decode('utf-8')
 
-            # img_list = Image.frombytes("L",(3,2),image_bytes)
-            # img_list = base64.b64decode(image_bytes).decode('latin-1')
-
-            print(img_list)
         
         data = {
             'attack' : attacks_info,
             'images':images,
-            'img_list' : image_bytes
+            'img_list' : img_list
         }
+
         return render(request, 'index.html', data)
 
     return render(request, 'index.html')
